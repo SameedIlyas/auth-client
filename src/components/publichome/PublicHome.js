3777664a185365dc987a2ac3b80823e3dc3ed213
Sameed Ilyas <silyas.bese22seecs@seecs.edu.pk>
2024-08-09 20:14:28 +0500

    minor change

--- src/components/publichome/PublicHome.js
+++ src/components/publichome/PublicHome.js
@@ -9,7 +9,7 @@ const PublicHome = () => {
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md text-center">
           <h1 className="text-2xl font-bold mb-4">Log in First</h1>
           <p className="mb-4">You need to be logged in to access the full features of this website.</p>
-          <p>Please <a href="/login" className="text-blue-500 hover:underline">Log in</a> to continue.</p>
+          <p>Please Log in to continue.</p>
         </div>
       </div>
     </div>
